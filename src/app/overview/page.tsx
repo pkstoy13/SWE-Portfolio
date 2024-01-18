ff40d94257b0a97a4932ab9df05cbc1f850e71b5
Philip Stoyanov <pkstoyanov1@gmail.com>
2024-01-18 16:57:58 -0500

    figured out image issue and started overview

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -2,14 +2,25 @@ import Image from "next/image";
 
 export default function Overview() {
   return (
-    <div className="flex justify-around">
-      <Image
-        src="/assets/placeholder.png"
-        width={300}
-        height={300}
-        alt="placeholder"
-      />
-      <p>Hello I am Phil</p>
+    <div>
+      <div className="flex justify-center mx-auto mt-4">
+        <Image
+          src={require("../assets/side_pic.jpg")}
+          width={400}
+          height={300}
+          alt="Profile Picture"
+          className="border-8 border-double border-gray-700 rounded-3xl"
+        />
+        <div className="ml-4 mt-2">
+          <div className="  border-b-2 border-black">
+            <p>Overview</p>
+          </div>
+          <p>
+            I am a junior at the University of Delaware. <br />I am studying
+            Computer Science. I am an aspiring software engineer.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
